f35923ff631533ab0fbdca0df12093cf1e05a7da
Judy Gichoya <judywawira@gmail.com>
2018-01-13 14:27:25 -0500

    Working login and logout

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,20 @@
 
 import os
 
-from flask import render_template,Blueprint,flash,redirect,url_for,send_from_directory
+from flask import render_template,Blueprint,flash,redirect,url_for,send_from_directory,request
 from flask_bootstrap import __version__ as FLASK_BOOTSRAP_VERSION
 from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+from flask_login import login_user, logout_user, current_user, login_required 
+from werkzeug.urls import url_parse
+
 from markupsafe import escape
 
 from PIL import Image
 from io import StringIO
 
-from app import app
-from app.forms import LoginForm,XrayForm
+from app import app, db
+from app.forms import LoginForm, XrayForm
+from app.models import User
 
 def serve_pil_image(pil_img):
     img_io = StringIO()
@@ -36,14 +40,17 @@ def index():
     return render_template('index.html', title= 'Nyumbani',user = user,posts=posts)
 
 @app.route('/worklist')
+@login_required
 def worklist():
     return render_template('worklist.html')
 
 @app.route('/stats')
+@login_required
 def stats():
     return render_template('stats.html')
 
 @app.route('/study/<study_id>')
+@login_required
 def study(study_id):
     studyid = study_id
     form = XrayForm()
@@ -52,16 +59,23 @@ def study(study_id):
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        #...
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember = form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template("login.html",title="Sign In", form=form)
- 
-    """
 
-    #img = Image.new('RGB',"/Users/judywawira/Programming/RadAIJournal/00006484_000.png")
-    #file_name = "/Users/judywawira/Programming/RadAIJournal/00006484_000.png"
-    #return serve_pil_image(img)
-    #return send_from_directory('/Users/judywawira/Programming/RadAIJournal','00006484_000.png')
-    """
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #used to protect against CSRF
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    #SQLite configurations 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
